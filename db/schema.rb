2cec123e087e528238d6877c803af59aa804bdbc
AnthoXLMA <contact@service-lite-plus.com>
2020-08-17 17:52:52 +0200

    fixed shemas

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_08_17_142347) do
+ActiveRecord::Schema.define(version: 2020_08_17_175106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,18 +43,6 @@ ActiveRecord::Schema.define(version: 2020_08_17_142347) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "last_name"
-    t.string "first_name"
-    t.string "phone_number"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "rentals", "trees"
-  add_foreign_key "rentals", "users"
-  add_foreign_key "trees", "users"
-
-  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -63,9 +50,14 @@ ActiveRecord::Schema.define(version: 2020_08_17_142347) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
+  add_foreign_key "rentals", "trees"
+  add_foreign_key "rentals", "users"
+  add_foreign_key "trees", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# require 'faker'
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
+
+# # Seed pour USER Booker
+# Booker.create(name: "Bobby")
+# Booker.create(name: "Michel")
+# Booker.create(name: "Germain")
+
+# # Seed pour USER Owner
+# Owner.create(name: "Jack")
+# Owner.create(name: "Johnson")
+# Owner.create(name: "Ben")
+
+
+# # Seed pour OWNER Tree
+# Tree.create(name: "Baobab")
+# Tree.create(name: "Chene")
+# Tree.create(name: "Cactus")
+
+# # Seed pour Rental
+# puts 'Creating rentals...'
+# baobab = Rental.new(name: "Baobab")
+# baobab.save!
+# cactus = Rental.new(name: "Cactus")
+# cactus.save!
+# puts 'Finished!'
+
+
+# require 'faker'
+
+# puts 'Creating 100 fake users...'
+# 100.times do
+#   booker = User.new(
+#     name:    Faker::Company.name,
+#     address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+#   )
+#   booker.save!
+# end
+# puts 'Finished!'
