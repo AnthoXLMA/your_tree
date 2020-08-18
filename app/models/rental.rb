49582c152c04cbfc8a96cf9cc0fb908cac2e1d74
AnthoXLMA <contact@service-lite-plus.com>
2020-08-18 17:58:54 +0200

    Comment validation

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,7 +1,7 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :tree
-  validates :start_on, :end_on, :total_price, :status, presence: true
-  validates :start_on, :end_on, format: { with: /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/ }
-  validates :total_price, numericality: true
+  # validates :start_on, :end_on, :total_price, :status, presence: true
+  # validates :start_on, :end_on, format: { with: /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/ }
+  # validates :total_price, numericality: true
 end
