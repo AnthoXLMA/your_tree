6c0e2981f91f35d6524a20302bb632972edb1e25
AnthoXLMA <contact@service-lite-plus.com>
2020-08-20 15:48:17 +0200

    Rentals-requests OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :rentals
   has_many :trees
+  has_many :rental_requests, through: :trees, source: :rentals
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
