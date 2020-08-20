889f75c99a69721ff3098412c7210fec60e13a8f
AnthoXLMA <contact@service-lite-plus.com>
2020-08-20 18:22:51 +0200

    Defining changes status OK

--- app/controllers/owner/rentals_controller.rb
+++ app/controllers/owner/rentals_controller.rb
@@ -3,5 +3,19 @@ module Owner
     def index
       @rentals = current_user.rental_requests
     end
+
+    def accept
+      @rental = Rental.find(params[:id])
+      @rental.status = 'accepted'
+      @rental.save
+      redirect_to owner_rentals_path
+    end
+
+    def refuse
+      @rental = Rental.find(params[:id])
+      @rental.status = 'refused'
+      @rental.save
+      redirect_to owner_rentals_path
+    end
   end
 end
