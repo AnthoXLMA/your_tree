17ae7fb38d3a76ad9cb63283d50bcc1157ab7a27
AnthoXLMA <contact@service-lite-plus.com>
2020-08-21 16:40:17 +0200

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,7 +54,7 @@ baobab = Tree.new(
   weight: "66lb",
   description: "A typical tree of dry tropical Africa, the baobab is the emblem of Senegal. This giant of the African savannas cannot survive in the open ground in mainland France. On the other hand, it is possible to welcome a baobab at home because a young plant can be grown as an indoor plant!",
   price: 5,
-  user_id: marco.id
+  user_id: bill.id
   )
 baobab.photo.attach(io: File.open(Rails.root.join('db/fixtures/trees/Baobab.jpeg')), filename: 'Baobab.jpeg')
 baobab.save!
@@ -68,7 +68,7 @@ cactus = Tree.new(
   weight: "440lb",
   description: "The cactus belongs to the family Cactaceae, which encompasses almost 2,000 different species, according to the Aggie Horticulture website of Texas A&M University. The cactus is an iconic representation of desert plant life, known for its prickly needles and often towering height.",
   price: 5,
-  user_id: marco.id
+  user_id: bill.id
   )
 cactus.photo.attach(io: File.open(Rails.root.join('db/fixtures/trees/Cactus.jpeg')), filename: 'Cactus.jpeg')
 cactus.save!
@@ -81,7 +81,7 @@ sapin = Tree.new(
   weight: "1322lb",
   description: "The fir trees are trees conifers of the genus Abies originating in temperate regions of the northern hemisphere. They are part of the Pinaceae family . They are recognizable by the way the needles attach to the shank, their shapes which differ from spruce, and their erect cones which disintegrate at maturity.",
   price: 10,
-  user_id: marco.id
+  user_id: bill.id
   )
 sapin.photo.attach(io: File.open(Rails.root.join('db/fixtures/trees/Sapin.jpeg')), filename: 'Sapin.jpeg')
 sapin.save!
@@ -94,23 +94,10 @@ pin = Tree.new(
   weight: "1322lb",
   description: "A pine is any conifer in the genus Pinus of the family Pinaceae. Pinus is the sole genus in the subfamily Pinoideae. The Plant List compiled by the Royal Botanic Gardens, Kew and Missouri Botanical Garden accepts 126 species names of pines as current, together with 35 unresolved species and many more synonyms. Pine may also refer to the lumber derived from pine trees; pine is one of the more extensively used types of wood used as lumber.",
   price: 10,
-  user_id: marco.id
+  user_id: jean.id
   )
 pin.photo.attach(io: File.open(Rails.root.join('db/fixtures/trees/Pin.jpeg')), filename: 'Pin.jpeg')
 pin.save!
-
-mimosa = Tree.new(
-  name: "Mimosa",
-  species: "Summer tree",
-  address: "79 boulevard de la Liberation, 13014 Marseille",
-  size: "medium",
-  weight: "440lb",
-  description: "Mimosa is a genus of trees of the Mimosaceae familyaccording to the classical classification of Cronquist (1981) or of the great family of Fabaceae and of the subfamily of Mimosoideae according to the phylogenetic classification. It includes nearly 400 species.",
-  price: 5,
-  user_id: marco.id
-  )
-mimosa.photo.attach(io: File.open(Rails.root.join('db/fixtures/trees/Mimosa.jpeg')), filename: 'Mimosa.jpeg')
-mimosa.save!
 
 chene = Tree.new(
   name: "Chêne",
@@ -120,7 +107,7 @@ chene = Tree.new(
   weight: "1322lb",
   description: "Oak is the vernacular name for many species of trees and shrubs belonging to the genus Quercus , and some related genera of the Fagaceae family , notably Cyclobalanopsis and Lithocarpus .",
   price: 10,
-  user_id: marco.id
+  user_id: jean.id
   )
 chene.photo.attach(io: File.open(Rails.root.join('db/fixtures/trees/Chene.jpeg')), filename: 'Chene.jpeg')
 chene.save!
@@ -133,7 +120,7 @@ acacia = Tree.new(
   weight: "1322lb",
   description: "Acacia, commonly known as the wattles or acacias, is a large genus of shrubs and trees in the subfamily Mimosoideae of the pea family Fabaceae. Initially, it comprised a group of plant species native to Africa and Australia.",
   price: 10,
-  user_id: marco.id
+  user_id: bill.id
   )
 acacia.photo.attach(io: File.open(Rails.root.join('db/fixtures/trees/Acacia.jpeg')), filename: 'Acacia.jpeg')
 acacia.save!
@@ -146,7 +133,7 @@ palmier = Tree.new(
   weight: "1322lb",
   description: "A palm tree is a plant of the Arecaceae botanical family , well known by date palms and coconut palms . Palm trees take the form of perennial lianas , shrubs and trees . They are the only members of the only Arecaceae family in the order Arecales. They grow in hot climates.",
   price: 10,
-  user_id: marco.id
+  user_id: bill.id
   )
 palmier.photo.attach(io: File.open(Rails.root.join('db/fixtures/trees/Palmier.jpeg')), filename: 'Palmier.jpeg')
 palmier.save!
@@ -159,7 +146,7 @@ pommier = Tree.new(
   weight: "1322lb",
   description: "The apple trees are trees of the genus botanical Malus and the family of Rosaceae , the fruit is the apple . This genus includes some forty species of trees or shrubs , the most important of which, in terms of human nutrition , is the domestic apple tree ( Malus domestica ). Today we know more than 20,000 varieties.",
   price: 10,
-  user_id: marco.id
+  user_id: jean.id
   )
 pommier.photo.attach(io: File.open(Rails.root.join('db/fixtures/trees/Pommier.jpg')), filename: 'Pommier.jpg')
 pommier.save!
